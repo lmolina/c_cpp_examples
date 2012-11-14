afc578e05fb5038c3a633c2ed246fe7c2021229b
Laudin Alessandro Molina T <laudin.molina@gmail.com>
2012-11-14 10:39:40 -0430

    Agregada versión inicial de programa ppal de agenda

--- agenda/main.cpp
+++ agenda/main.cpp
@@ -1,60 +1,131 @@
 # include <iostream>
-# include <string>
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
 # include "persona.h"
 # include "agenda.h"
+//# include "corregir.h"
+
+enum Opciones {mostrar = 0, agregar, eliminar, buscar_nombre, buscar_apellido,
+    buscar_telefono, salir};
+
+int menu(Agenda & agenda);
+void buscar_nombre(Agenda & agenda);
+void buscar_apellido(Agenda & agenda);
+void buscar_telefono(Agenda & agenda);
+void mostrar_agenda(Agenda & agenda);
+void agregar_entrada(Agenda & agenda);
 
 int main() {
-  Persona ej1;
-  Persona ej2("Laudin", "Molina", 555555);
+    Agenda agenda;
 
+    cargar_agenda(agenda);
+    menu(agenda);
+    //int nota = corregir(agenda);
+    //cout << nota << endl;
 
-  ej1.mostrar();
-  cout << endl;
-
-  ej1.nombre("Alessandro");
-  ej1.apellido("Troconis");
-  ej1.telefono(111222444);
-  ej1.mostrar();
-  cout << endl;
-
-  ej2.mostrar();
-  cout << endl;
-
-  cout << endl;
-  cout << "XXXXXXXX Agenda XXXXXXXXXX" << endl;
-  cout << endl;
-  Agenda agenda;
-  agenda.mostrar();
-
-  agenda.agregar_persona(ej1);
-  agenda.agregar_persona(ej2);
-  agenda.mostrar();
-
-  cout << endl;
-  cout << "Buscar" << endl;
-  string nombre = "Laudin";
-  vector< Persona >::iterator it;
-  vector< Persona > r = agenda.buscar_nombre(nombre);
-  for (it = r.begin(); it < r.end(); ++it) {
-    it->mostrar();
-    cout << endl;
-  }
-
-  agenda.agregar_persona(Persona("Laudin", "Troconis", 123));
-  cout << endl;
-  cout << "Buscar" << endl;
-  nombre = "Laudin";
-  r = agenda.buscar_nombre(nombre);
-  for (it = r.begin(); it < r.end(); ++it) {
-    it->mostrar();
-    cout << endl;
-  }
-    
-  cout << endl << endl;
-  agenda.eliminar_persona(ej2);
-  agenda.mostrar();
-
-  return 0;
+    return 0;
 }
+
+
+int menu(Agenda & agenda) {
+    int opc;
+    do {
+        cout << "****************" << endl
+            << "Agenda" << endl
+            << "Buscar nombre" << nombre << endl
+            << "Buscar apellido" << apellido << endl
+            << "Buscar telefono" << telefono << endl
+            << "Mostrar agenda" << mostrar << endl
+            << "Agregar entrada" << agregar << endl
+            << endl
+            << "Salir" << salir << endl;
+        cin >> opc;
+
+        switch(opc) {
+            case nombre:
+                buscar_nombre(agenda);
+                break;
+            case apellido
+                buscar_apellido(agenda);
+                break;
+            case telefono:
+                buscar_telefono(agenda);
+                break;
+            case mostrar:
+                mostrar_agenda(agenda);
+                break;
+            case agregar:
+                agregar_entrada(agenda);
+                break;
+            case salir:
+                cout << "Ciao!" << endl;
+                break;
+            default:
+                cerr << "Opcion no valida" << endl;
+        }
+    } while (opc != salir);
+}
+
+void buscar_nombre(Agenda & agenda) {
+    string nombre;
+
+    cout << "Nombre? ";
+    cin >> nombre;
+    vector< Persona > r = agenda.buscar_nombre(nombre);
+    vector< Persona >::iterator it;
+
+    for(it = r.begin(); it < r.end(); ++it) {
+        it->mostrar();
+        cout << endl;
+    }
+}
+
+void buscar_apellido(Agenda & agenda) {
+    string apellido;
+
+    cout << "Apellido? ";
+    cin >> apellido;
+    vector< Persona > r = agenda.buscar_apellido(apellido);
+    vector< Persona >::iterator it;
+
+    for(it = r.begin(); it < r.end(); ++it) {
+        it->mostrar();
+        cout << endl;
+    }
+}
+
+void buscar_telefono(Agenda & agenda) {
+    long telefono;
+    cout << "Telefono? ";
+    cin >> telefono;
+
+    Persona r = agenda.buscar_telefono(telefono);
+    if (r.telefono() > 0)
+        r.mostrar();
+    else
+        cout << "Registro no encontrado" << endl;
+    cout << endl;
+}
+
+void mostrar_agenda(Agenda & agenda) {
+    agenda.mostrar();
+}
+
+void agregar_entrada(Agenda & agenda) {
+    string nombre;
+    string apellido;
+    long telefono;
+
+    cout << "Ingresar datos:" << endl;
+    cout << "Nombre? ";
+    cin >> nombre;
+    cout << "Apellido? ";
+    cin >> apellido;
+    cout << "Telefono? ";
+    cin >> telefono;
+
+    agenda.agregar_persona(Persona(nombre, apellido, telefono));
+}
+ 
---
+++ agenda/prueba.cpp
@@ -0,0 +1,60 @@
+# include <iostream>
+# include <string>
+using namespace std;
+
+# include "persona.h"
+# include "agenda.h"
+
+int main() {
+  Persona ej1;
+  Persona ej2("Laudin", "Molina", 555555);
+
+
+  ej1.mostrar();
+  cout << endl;
+
+  ej1.nombre("Alessandro");
+  ej1.apellido("Troconis");
+  ej1.telefono(111222444);
+  ej1.mostrar();
+  cout << endl;
+
+  ej2.mostrar();
+  cout << endl;
+
+  cout << endl;
+  cout << "XXXXXXXX Agenda XXXXXXXXXX" << endl;
+  cout << endl;
+  Agenda agenda;
+  agenda.mostrar();
+
+  agenda.agregar_persona(ej1);
+  agenda.agregar_persona(ej2);
+  agenda.mostrar();
+
+  cout << endl;
+  cout << "Buscar" << endl;
+  string nombre = "Laudin";
+  vector< Persona >::iterator it;
+  vector< Persona > r = agenda.buscar_nombre(nombre);
+  for (it = r.begin(); it < r.end(); ++it) {
+    it->mostrar();
+    cout << endl;
+  }
+
+  agenda.agregar_persona(Persona("Laudin", "Troconis", 123));
+  cout << endl;
+  cout << "Buscar" << endl;
+  nombre = "Laudin";
+  r = agenda.buscar_nombre(nombre);
+  for (it = r.begin(); it < r.end(); ++it) {
+    it->mostrar();
+    cout << endl;
+  }
+    
+  cout << endl << endl;
+  agenda.eliminar_persona(ej2);
+  agenda.mostrar();
+
+  return 0;
+}
