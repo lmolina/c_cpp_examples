e88f774186dd9e283a9d7e9a914d01dbc23525a7
Laudin Alessandro Molina T <laudin.molina@gmail.com>
2012-11-26 10:26:57 -0430

    Solución a cuenta_dias.cpp

--- cuenta_dias.cpp
+++ cuenta_dias.cpp
@@ -1,9 +1,15 @@
 /*
+ * Contar el número de dias que han transcurrido desde el 1 de enero hasta una
+ * fecha indicada por el usuario.
  *
+ * Copyright 2012 Laudin Molina T <laudi@ula.ve>
  */
 
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::cin;
 
 bool es_anyo_bisiesto(int anyo);
 bool es_fecha_valida(int anyo, int mes, int dia);
@@ -24,31 +30,83 @@ int main() {
       cout << "Dias transcurridos desde el 1/1/" << anyo
           << " hasta el " << dia << "/" << mes << "/" << anyo << ": "
           << dias_desde_inicio_de_anyo(anyo, mes, dia) << endl;
-    }
-    else {
+    } else {
       cerr << "Fecha invalida" << endl;
     }
-    
+
     return 0;
 }
 
 bool es_anyo_bisiesto(int anyo) {
+  bool bisiesto;
 
+  if ((anyo % 4 == 0 && anyo % 100 != 0) || anyo % 400 == 0) {
+    bisiesto = true;
+  } else {
+    bisiesto = false;
+  }
+
+  return bisiesto;
 }
 
 
 bool es_fecha_valida(int anyo, int mes, int dia) {
+  bool valido;
+  int febrero;
+
+  if (es_anyo_bisiesto(anyo))
+    febrero = 29;
+  else
+    febrero = 28;
+
+  if (dia >= 1 && mes >= 1 && mes <= 12 && anyo >= 0) {
+    switch (mes) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        if (dia <= 31)
+          valido = true;
+        else
+          valido = false;
+        break;
+
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        if (dia <= 30)
+          valido = true;
+        else
+          valido = false;
+        break;
+
+      case 2:
+        if (dia <= febrero)
+          valido = true;
+        else
+          valido = false;
+        break;
+    }
+  } else {
+    valido = false;
+  }
+
+  return valido;
 }
 
 int dias_desde_inicio_de_anyo(int anyo, int mes, int dia) {
   int enero, febrero, marzo, abril, mayo, junio, julio, agosto, septiembre,
-      octubre, noviembre, diciembre; 
+      octubre, noviembre, diciembre;
 
   // Dias que tiene cada mes
   enero = 31;
 
   // Febrero es especial, puede tener 28 o 29 dias
-  if (es_anyo_bisiesto(anyo)) 
+  if (es_anyo_bisiesto(anyo))
     febrero = 29;
   else
     febrero = 28;
@@ -66,19 +124,19 @@ int dias_desde_inicio_de_anyo(int anyo, int mes, int dia) {
 
   int dias_transcurridos = 0;
   switch (mes) {
-    case 1: 
+    case 1:
       dias_transcurridos = dia;
       break;
-    case 2: 
+    case 2:
       dias_transcurridos = enero + dia;
       break;
-    case 3: 
+    case 3:
       dias_transcurridos = enero + febrero + dia;
       break;
     case 4:
       dias_transcurridos = enero + febrero + marzo + dia;
       break;
-    case 5: 
+    case 5:
       dias_transcurridos = enero + febrero + marzo + abril + dia;
       break;
     case 6:
@@ -88,21 +146,26 @@ int dias_desde_inicio_de_anyo(int anyo, int mes, int dia) {
       dias_transcurridos = enero + febrero + marzo + abril + mayo + junio + dia;
       break;
     case 8:
-      dias_transcurridos = enero + febrero + marzo + abril + mayo + junio + julio + dia;
+      dias_transcurridos = enero + febrero + marzo + abril + mayo + junio
+          + julio + dia;
       break;
     case 9:
-      dias_transcurridos = enero + febrero + marzo + abril + mayo + junio + julio + agosto + dia;
+      dias_transcurridos = enero + febrero + marzo + abril + mayo + junio
+          + julio + agosto + dia;
       break;
     case 10:
-      dias_transcurridos = enero + febrero + marzo + abril + mayo + junio + julio + agosto + septiembre + dia;
+      dias_transcurridos = enero + febrero + marzo + abril + mayo + junio
+          + julio + agosto + septiembre + dia;
       break;
     case 11:
-      dias_transcurridos = enero + febrero + marzo + abril + mayo + junio + julio + agosto + septiembre + octubre + dia;
+      dias_transcurridos = enero + febrero + marzo + abril + mayo + junio
+          + julio + agosto + septiembre + octubre + dia;
       break;
     case 12:
-      dias_transcurridos = enero + febrero + marzo + abril + mayo + junio + julio + agosto + septiembre + octubre + noviembre + dia;
+      dias_transcurridos = enero + febrero + marzo + abril + mayo + junio
+          + julio + agosto + septiembre + octubre + noviembre + dia;
       break;
   }
 
-  return dias_transcurridos;
+  return dias_transcurridos - 1;
 }
